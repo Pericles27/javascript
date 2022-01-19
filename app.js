8fbf74cb5afa5875c0c0e39f67638b29f0c4f0a2
Nicolás <nicolasherrera231@gmail.com>
2022-01-19 19:49:16 -0300

    Pruebas

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-for (i = 1; i<=5 ; i++){
-    let numero = parseInt(prompt("Ingresa un numero: "));
-    let resultado = numero * i;
-    alert("el numero ingresado multiplicado por el numero de repeticion da: "+ resultado)
+function multiplicar(x,y){
+    let resultado = x*y;
+    alert(resultado +"!")
 }
+function saludar(){
+    let nombre=prompt("Dime tu nombre: ")
+    alert("Bienvenido, "+nombre+"!")
+}
+saludar()
+for (let i = 1; i<=5 ; i++){
+    let numero = parseInt(prompt("Ingresa un numero: "));
+    alert("el numero ingresado multiplicado por el numero de repeticion da . . .  ")
+    multiplicar(numero,i)
+}
+
 let num2= parseInt(prompt("Ingresa un numero menor a 100: "));
 while (num2 <100){
-    let res = num2*5
-    alert("El numero multiplicado por 5: " + res)
-    num2= parseInt(prompt("Ingresa un numero menor a 100: "));
+    alert("El numero multiplicado por 5 da . . . ")
+    multiplicar(num2,5)
+    num2= parseInt(prompt("ingresa otro numero menor a 100, para terminar ingresa un numero mayor a 100: "));
 }
 alert("fin de la actividad")
 
