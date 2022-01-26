9146e16100aafb84bcd516e4db82d8e773765b17
Nicolás <nicolasherrera231@gmail.com>
2022-01-26 19:12:30 -0300

    entrega

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-function productos (nombre,precio,cant,codigo) {
-    this.nombre =nombre;
-    this.precio = precio;
-    this.cantidad = cant;
-    this.codigo = codigo
+class productos{
+    constructor(nombre,precio,cant,codigo) {
+        this.nombre =nombre;
+        this.precio = precio;
+        this.cantidad = cant;
+        this.codigo = codigo
+    }
+    iva(){
+        this.precio=this.precio *1.21
+    }
 }
 function nombre(){
     let nom = prompt("Ingresa tu nombre: ")
@@ -15,10 +20,83 @@ function saludar(x){
         alert("Hasta luego "+nomb)
     }
 }
+function nombre_producto(v){
+    if (v =="PS5"){
+        let t=  PS5.nombre;
+        return t
+    } else if (v=="XBS"){
+        let t = XBS.nombre
+        return t
+    }else if (v=="DLS"){
+        let t = DLS.nombre
+        return t
+    } else if (v=="COD") {
+        let t = COD.nombre
+        return t
+    }else if (v=="NSW"){
+        let t = NSW.nombre
+        return t
+    } else {
+        alert("El codigo ingresado no corresponde a ninguno de nuestros productos en venta")
+    }
+}
+function cantidad_cuotas(v){
+    if (v =="PS5"){
+        let t=  PS5.cantidad;
+        return t
+    } else if (v=="XBS"){
+        let t = XBS.cantidad
+        return t
+    }else if (v=="DLS"){
+        let t = DLS.cantidad
+        return t
+    } else if (v=="COD") {
+        let t = COD.cantidad
+        return t
+    }else if (v=="NSW"){
+        let t = NSW.cantidad
+        return t
+    } else {
+        alert("El codigo ingresado no corresponde a ninguno de nuestros productos en venta")
+    }
+}
+function pre(v){
+    if (v =="PS5"){
+        let t=  PS5.precio;
+        return t
+    } else if (v=="XBS"){
+        let t = XBS.precio
+        return t
+    }else if (v=="DLS"){
+        let t = DLS.precio
+        return t
+    } else if (v=="COD") {
+        let t = COD.precio
+        return t
+    }else if (v=="NSW"){
+        let t = NSW.precio
+        return t
+    } else {
+        alert("El codigo ingresado no corresponde a ninguno de nuestros productos en venta")
+    }
+}
+function precio_iva(v){
+    if (v =="PS5"){
+        PS5.iva();
+    } else if (v=="XBS"){
+        XBS.iva()
+    }else if (v=="DLS"){
+        DLS.iva()
+    } else if (v=="COD") {
+        COD.iva()
+    }else if (v=="NSW"){
+        NSW.iva()
+    }
+}
 function cuo(x){
-    alert("Puedes pagar este producto en hasta "+(x).cantidad+" de cuotas sin interes")
+    alert("Puedes pagar este producto en hasta "+x+" de cuotas sin interes")
     let c =parseInt(prompt("En cuantas cuotas quieres pagar: "))
-    if (c>(x).cantidad){
+    if (c>x){
         let c =parseInt(prompt("No tenemos esa cantidad de pagos disponible, ingresa una menor: "))
         return c
     } else if (c<0){
@@ -30,9 +108,14 @@ function cuo(x){
 }
 function venta(){
     let v =prompt("¿Cual producto deseas comprar?: Ingresa el codigo del producto")
-    let cuotas = cuo(v)
-    let final = dividir(v.precio,cuotas)
-    alert("Podrás pagar tu "+v.nombre+" en "+cuotas+" de $"+final)
+    precio_iva(v)
+    let p = pre(v)
+    alert("El precio del producto con el iva es de: $"+p)
+    let x = cantidad_cuotas(v)
+    let cuotas = cuo(x)
+    let final = dividir(p,cuotas)
+    let f = nombre_producto(v)
+    alert(nomb+", podrás pagar tu "+f+" en "+cuotas+" cuotas de $"+final)
 }
 function preg(){
     alert("Encontraras nuestro catalogo en la consola: ")
@@ -52,6 +135,6 @@ const NSW = new productos("Nintendo Switch",125000,9,"NSW")
 let nomb= nombre()
 saludar(0)
 preg()
-
+saludar()
 
 
