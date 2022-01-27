b56b6e6e07095d4b43fc67d3fa7d6a644f0d3d02
Nicolás <nicolasherrera231@gmail.com>
2022-01-26 22:49:43 -0300

    Entrega

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ class productos{
         this.nombre =nombre;
         this.precio = precio;
         this.cantidad = cant;
-        this.codigo = codigo
+        this.codigo = codigo;
+        this.vendido = false;
     }
     iva(){
-        this.precio=this.precio *1.21
+        this.precio=this.precio *1.21;
     }
 }
 function nombre(){
@@ -17,81 +18,16 @@ function saludar(x){
     if (x==0){
         alert("Bienvenido al simulador "+nomb)
     } else{
-        alert("Hasta luego "+nomb)
+        alert("Hasta luego "+nomb+" ¡gracias por tu compra!")
     }
 }
-function nombre_producto(v){
-    if (v =="PS5"){
-        let t=  PS5.nombre;
-        return t
-    } else if (v=="XBS"){
-        let t = XBS.nombre
-        return t
-    }else if (v=="DLS"){
-        let t = DLS.nombre
-        return t
-    } else if (v=="COD") {
-        let t = COD.nombre
-        return t
-    }else if (v=="NSW"){
-        let t = NSW.nombre
-        return t
-    } else {
-        alert("El codigo ingresado no corresponde a ninguno de nuestros productos en venta")
-    }
+function cantidad_cuotas(v) {
+    const n = lista_productos.find(n => n.codigo == v)
+    return (n.cantidad)
 }
-function cantidad_cuotas(v){
-    if (v =="PS5"){
-        let t=  PS5.cantidad;
-        return t
-    } else if (v=="XBS"){
-        let t = XBS.cantidad
-        return t
-    }else if (v=="DLS"){
-        let t = DLS.cantidad
-        return t
-    } else if (v=="COD") {
-        let t = COD.cantidad
-        return t
-    }else if (v=="NSW"){
-        let t = NSW.cantidad
-        return t
-    } else {
-        alert("El codigo ingresado no corresponde a ninguno de nuestros productos en venta")
-    }
-}
-function pre(v){
-    if (v =="PS5"){
-        let t=  PS5.precio;
-        return t
-    } else if (v=="XBS"){
-        let t = XBS.precio
-        return t
-    }else if (v=="DLS"){
-        let t = DLS.precio
-        return t
-    } else if (v=="COD") {
-        let t = COD.precio
-        return t
-    }else if (v=="NSW"){
-        let t = NSW.precio
-        return t
-    } else {
-        alert("El codigo ingresado no corresponde a ninguno de nuestros productos en venta")
-    }
-}
-function precio_iva(v){
-    if (v =="PS5"){
-        PS5.iva();
-    } else if (v=="XBS"){
-        XBS.iva()
-    }else if (v=="DLS"){
-        DLS.iva()
-    } else if (v=="COD") {
-        COD.iva()
-    }else if (v=="NSW"){
-        NSW.iva()
-    }
+function pre(v) {
+    const n = lista_productos.find(n => n.codigo == v)
+    return (n.precio)
 }
 function cuo(x){
     alert("Puedes pagar este producto en hasta "+x+" de cuotas sin interes")
@@ -106,9 +42,15 @@ function cuo(x){
         return c
     }
 }
+function nombre_producto(v) {
+    const n = lista_productos.find(n => n.codigo == v)
+    return (n.nombre)
+}
 function venta(){
     let v =prompt("¿Cual producto deseas comprar?: Ingresa el codigo del producto")
-    precio_iva(v)
+    for (productos of lista_productos){
+        productos.iva()
+    }
     let p = pre(v)
     alert("El precio del producto con el iva es de: $"+p)
     let x = cantidad_cuotas(v)
@@ -119,11 +61,9 @@ function venta(){
 }
 function preg(){
     alert("Encontraras nuestro catalogo en la consola: ")
-    console.log(PS5.nombre+": "+"$"+PS5.precio+" Codigo del producto: "+PS5.codigo)
-    console.log(XBS.nombre+": "+"$"+XBS.precio+" Codigo del producto: "+XBS.codigo)
-    console.log(DLS.nombre+": "+"$"+DLS.precio+" Codigo del producto: "+DLS.codigo)
-    console.log(COD.nombre+": "+"$"+COD.precio+" Codigo del producto: "+COD.codigo)
-    console.log(NSW.nombre+": "+"$"+NSW.precio+" Codigo del producto: "+NSW.codigo)
+    for (const producto of lista_productos) {
+        console.log(producto.nombre+": $"+producto.precio+" Codigo del producto: "+producto.codigo)
+    }
     venta()
 }
 const dividir = (x,y) => x/y;
@@ -132,9 +72,15 @@ const XBS = new productos("XBox Series S",140000,9,"XBS")
 const DLS = new productos("Dualsense",15000,6,"DLS")
 const COD = new productos("Call of duty",12000,12,"COD")
 const NSW = new productos("Nintendo Switch",125000,9,"NSW")
+const lista_productos=[PS5,XBS,DLS,COD,NSW]
+lista_productos.sort(function (a, b) {
+    return (a.precio - b.precio)
+})
 let nomb= nombre()
 saludar(0)
 preg()
 saludar()
+
+
 
 
