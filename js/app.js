1bd3ebb92081e5f858a9247a8ca004e1b8ba1096
Nicolás <nicolasherrera231@gmail.com>
2022-02-16 19:51:30 -0300

    pulido

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-class productos{
+class Producto{
     constructor(nombre,precio,cant,codigo) {
         this.nombre =nombre;
         this.precio = precio;
@@ -10,66 +10,62 @@ class productos{
         this.precio=this.precio *1.21;
     }
 }
+
 function cantidad_cuotas(producto) {
     const cuotas = lista_productos.find(cuotas => cuotas.codigo == producto)
     return (cuotas.cantidad)
 }
-function pre(producto) {
+
+function precio_del_producto(producto) {
     const cod = lista_productos.find(cod => cod.codigo == producto)
     return (cod.precio)
 }
+
 function nombre_producto(producto) {
-    const pro = lista_productos.find(pro => pro.codigo == producto)
-    return (pro.nombre)
+    const prod = lista_productos.find(prod => prod.codigo == producto)
+    return (prod.nombre)
 }
 
 const dividir = (x,y) => x/y;
-const PS5 = new productos("PlayStation 5",150000,12,"PS5")
-const XBS = new productos("XBox Series S",140000,9,"XBS")
-const DLS = new productos("Dualsense",15000,6,"DLS")
-const COD = new productos("Call of duty",12000,12,"COD")
-const NSW = new productos("Nintendo Switch",125000,9,"NSW")
+const PS5 = new Producto("PlayStation 5",150000,12,"PS5")
+const XBS = new Producto("XBox Series S",140000,9,"XBS")
+const DLS = new Producto("Dualsense",15000,6,"DLS")
+const COD = new Producto("Call of duty",12000,12,"COD")
+const NSW = new Producto("Nintendo Switch",125000,9,"NSW")
 const lista_productos=[PS5,XBS,DLS,COD,NSW]
 
-let boton = document.getElementById("boton")
-boton.onclick = () =>{enviarAlerta(respuesta())}
 
-function respuesta(){
-    let res = document.getElementById("opcion").value;
-    return res
-}
+let boton = document.getElementById("boton")
+boton.onclick = () =>{enviarAlerta(document.getElementById("opcion").value)}
+
 function enviarAlerta(codigo){
-    for (productos of lista_productos){
-        productos.iva()
+    for (Producto of lista_productos){
+        Producto.iva()
     }
-    borrar()
-    crear(codigo)
-}
-function borrar(){
-    let contenido = document.getElementsByClassName("catalogo");
-    contenido[0].parentNode.removeChild(contenido[0]);
-    let content = document.getElementsByClassName("form");
-    content[0].parentNode.removeChild(content[0]);
-}
-function crear(codigo){
+    borrar_catalogo()
     let contenedor = document.createElement("div");
     contenedor.innerHTML = `<h3> ID: ${codigo}</h3>
                             <h3>  Producto: ${nombre_producto(codigo)}</h3>
-                            <p> Precio del producto con el IVA incluido:  $${pre(codigo)}</p>
+                            <p> Precio del producto con el IVA incluido:  $${precio_del_producto(codigo)}</p>
                             <p>Cantidad de cuotas sin interes disponibles: ${cantidad_cuotas(codigo)}
                             <br><br>
-                            <form class="form">
+                            <form class="Form">
                                 <label for="pagos">Ingresá la cantidad de cuotas en las que queres pagar:</label>
-                                <input type="number" name="pagos" min="1" max="${cantidad_cuotas(codigo)}">
-                                <button type="button" value="Enviar" id="boton2">Enviar</button>
+                                <input type="number" name="pagos" min="1" max="${cantidad_cuotas(codigo)}" id="pagos">
+                                <button type="submit">Enviar</button> 
                             </form>
                             `;
     document.body.appendChild(contenedor);
 }
 
-function finalizar(){
-    borrar()
+function borrar_catalogo(){
+    let contenido = document.getElementsByClassName("Catalogo");
+    contenido[0].parentNode.removeChild(contenido[0]);
+    let content = document.getElementsByClassName("Form");
+    content[0].parentNode.removeChild(content[0]);
 }
+
+
 
 
 
