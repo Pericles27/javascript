890cb8c16b34de67cfba97df3efe6215f7ed4b95
Nicolás <nicolasherrera231@gmail.com>
2022-03-18 23:40:22 -0300

    mas cambios

--- js/app.js
+++ js/app.js
@@ -26,32 +26,14 @@ function nombre_producto(producto) {
     return (prod.nombre)
 }
 
-const dividir = (x,y) => x/y;
-const PS5 = new Producto("PlayStation 5",150000,12,"PS5")
-const XBS = new Producto("XBox Series S",140000,9,"XBS")
-const DLS = new Producto("Dualsense",15000,6,"DLS")
-const COD = new Producto("Call of duty",12000,12,"COD")
-const NSW = new Producto("Nintendo Switch",125000,9,"NSW")
-const lista_productos=[PS5,XBS,DLS,COD,NSW]
-
 class Compra{
-    constructor(nro_compra,nombre,precio,cant,cuota) {
-        this.nro_compra = nro_compra;
+    constructor(nombre,precio,cant,cuota) {
         this.nombre =nombre;
         this.precio = precio;
         this.cuotas = cant;
         this.valor_cuotas= cuota;
     }
 }
-
-let carro =[]
-
-const guardarLocal = (nombre, valor) => { sessionStorage.setItem(nombre, valor) };
-
-$("#boton").click(function(){
-    enviarAlerta(document.getElementById("opcion").value)
-});
-
 
 function enviarAlerta(codigo){
     for (Producto of lista_productos){
@@ -76,29 +58,44 @@ function enviarAlerta(codigo){
                                 <input type="button" id="atras" value="Atras">
                             </fieldset>
                         </form>`);
-
+    $("#boton2").click(function(){
+        let produ = document.getElementById("pagos").value
+        let valor = dividir(precio_del_producto(codigo),produ)
+        let compra = new Compra(nombre_producto(codigo),precio_del_producto(codigo),produ,valor)
+        carro.push(compra)
+        $("#formulario").remove()
+        $("#producto").remove()
+        $(".Catalogo").fadeIn("fast")
+    });
     $("#atras").click(function(){
         $(".Catalogo").fadeIn("fast")
         $("#formulario").remove()
         $("#producto").remove()
     });
-    $("#boton2").click(function(nom){
-        let a = 1
-        let produ = document.getElementById("pagos").value
-        let valor = precio_del_producto(codigo)/produ
-        let compra = new Compra(a,nombre_producto(codigo),precio_del_producto(codigo),produ,valor)
-        carro.push(compra)
-        a = a +1
-        $("#formulario").remove()
-        $("#producto").remove()
-        $(".Catalogo").fadeIn("fast")
-    });
 }
 
+let endpoint = "https://api.bluelytics.com.ar/v2/latest"
+fetch(endpoint)
+    .then(response => response.json())
+    .then( data => mostrarData(data))
+    .catch( error => console.log(error))
+
+
+const mostrarData =(data)=> {
+    console.log(data)
+}
 
 function borrar_catalogo(){
     $(".Catalogo").fadeOut("fast")
 }
+
+let carro =[]
+
+const guardarLocal = (nombre, valor) => { localStorage.setItem(nombre, valor) };
+
+$("#boton").click(function(){
+    enviarAlerta(document.getElementById("opcion").value)
+});
 
 $("#PS5").click(function(){
     enviarAlerta("PS5")
@@ -121,8 +118,55 @@ $("#COD").click(function(){
 });
 
 $("#carro").click(function(){
-    console.log(carro)
+    borrar_catalogo()
+    for (const compra of carro){
+        $('body').append(`
+        <section id="producto">
+            <h1>Producto: ${compra.nombre}</h1>
+            <h3>precio total: ${compra.precio}</h3>
+            <h3>cantidad de cuotas: ${compra.cuotas}</h3>
+            <h3> valor de cada cuota: ${compra.valor_cuotas}</h3>
+        </section>`);
+    }
+    $('body').append(`
+    <input type="button" value="Ir a pagar" id="pagar">
+    `);
+    $("#pagar").click(function(){
+        $("#pagar").remove()
+        $('body').append(`
+        <form class="Form" id="formulario" method="post">
+            <fieldset>
+                <label>Ingrese su nombre: </label>
+                <input type="text" placeholder="Ingrese su nombre">
+                <br><br>
+                <label>Numero de documento</label>
+                <input type="text" placeholder="ingrese su numero de documento"
+                <br><br>
+                <label>Numero de tarjeta</label>
+                <input type="text" max-len=19 min-len=15 placeholder="Ingrese los numeros del frente de la tarjeta" required>
+                <br><br>
+                <input type="submit" id="final" value="Enviar">
+            </fieldset>
+        </form> 
+        `); 
+        $("#final").submit(function(){
+            let guardar = JSON.stringify(carro)
+            guardarLocal("carrito",guardar)
+        })
+    })
 })
+
+
+const dividir = (x,y) => x/y;
+const PS5 = new Producto("PlayStation 5",150000,12,"PS5")
+const XBS = new Producto("XBox Series S",140000,9,"XBS")
+const DLS = new Producto("Dualsense",15000,6,"DLS")
+const COD = new Producto("Call of duty",12000,12,"COD")
+const NSW = new Producto("Nintendo Switch",125000,9,"NSW")
+const lista_productos=[PS5,XBS,DLS,COD,NSW]
+
+
+
 
 
 
